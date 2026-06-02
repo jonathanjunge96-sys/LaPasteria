c6104c4ec49703e03b640ff07eb5ff9aaf349ad1
Jonathan Junge <jonathan.junge96@gmail.com>
2026-06-02 19:59:59 +0200

    Add mobile support

--- server/middleware/ValidateTokenHandler.js
+++ server/middleware/ValidateTokenHandler.js
@@ -1,18 +1,20 @@
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 
+// Vårt middleware som validerar JWT-tokens
+
 const validateToken = asyncHandler(async (req, res, next) => {
   let token;
   let authHeader = req.headers.authorization;
 
-  if (authHeader && authHeader.startsWith("Bearer")) {
+  if (authHeader && authHeader.startsWith("Bearer")) { // Kollar tecken efter Bearer
     token = authHeader.split(" ")[1];
-    if (!token) {
+    if (!token) { // Är token null -> Fortsätt som gäst
       return next();
     }
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
       if (err) {
-        return next();
+        return next(); // Kollar giltig token mot vår nyckel i .env. om ogiltig = gäst
       }
       req.user = decoded.user;
       next();
